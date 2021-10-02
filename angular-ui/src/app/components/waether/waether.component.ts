aa0139d366be1b05aeab827223818054f0aae995
Mario <kruczeck@wp.pl>
2021-10-02 15:57:25 +0200

    [change] - almost working Angular weather Forecast Client

--- angular-ui/src/app/components/waether/waether.component.ts
+++ angular-ui/src/app/components/waether/waether.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WeatherService} from "../../services/weather.service";
+import {SpringWeatherResponse} from "../../models/weather.data";
 
 @Component({
   selector: 'app-waether',
@@ -8,11 +9,18 @@ import {WeatherService} from "../../services/weather.service";
 })
 export class WaetherComponent implements OnInit {
   name: string = ""
+  response!: Array<SpringWeatherResponse>
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
     this.name = this.weatherService.myName()
+    this.weatherService.readWeatherForecasts()
+      .subscribe(value => {
+        this.response = value
+        console.log(`received value: ${value}`)
+      })
+    console.log('ngOnInit completed');
   }
 
 }
