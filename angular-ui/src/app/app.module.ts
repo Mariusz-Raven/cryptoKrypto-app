f0567ed89b0acb7af7f5523f3ab635a13285fa89
Mario <kruczeck@wp.pl>
2021-10-10 11:55:47 +0200

    [change] - routing implement, better links handling

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -7,18 +7,36 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import { GithubComponent } from './components/github/github.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import {githubUrl, weatherUrl} from "./models/links";
+
+const routes: Routes = [
+  {path: githubUrl, component: GithubComponent},
+  {path: weatherUrl, component: WeatherComponent},
+  {path: '', redirectTo: weatherUrl, pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherComponent,
-    GithubComponent
+    GithubComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
